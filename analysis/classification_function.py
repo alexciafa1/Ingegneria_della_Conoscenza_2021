5afa67f831e5d21e362a5c79378ae9de65c66105
beppeForziati <giuseppeforziati96@gmail.com>
2021-09-11 16:12:11 +0200

    inserimento best_params
    inserimento dei best_params per i classificatori commentati

--- analysis/classification_function.py
+++ analysis/classification_function.py
@@ -1,5 +1,6 @@
+import numpy as np
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 
@@ -7,7 +8,26 @@ from sklearn.neighbors import KNeighborsClassifier
 def knn_classification(training, target):
 
     x_train, x_test, y_train, y_test = train_test_split(training, target, test_size=0.3, random_state=0)
+    '''
+    knn = KNeighborsClassifier()
+    parameters_knn = {
+        'n_neighbors': (1, 10, 13),
+        'weights': ('uniform', 'distance'),
+        'metric': ('minkowski', 'manhattan')}
 
+    # GridSearch
+    grid_search_knn = GridSearchCV(
+        estimator=knn,
+        param_grid=parameters_knn,
+        scoring='accuracy',
+        n_jobs=-1,
+        cv=5
+    )
+
+    knn_1 = grid_search_knn.fit(x_train, y_train)
+    y_pred_knn1 = knn_1.predict(x_test)
+    print("Best params", grid_search_knn.best_params_)
+    '''
     knn = KNeighborsClassifier(metric='manhattan', n_neighbors=13, weights='distance')
 
     knn.fit(x_train, y_train)
@@ -24,6 +44,23 @@ def knn_classification(training, target):
 def gaussian_nb_classification(training, target):
 
     x_train, x_test, y_train, y_test = train_test_split(training, target, test_size=0.3, random_state=0)
+    '''
+    gau = GaussianNB()
+    parameters_gau = {'var_smoothing': np.logspace(0, -9, num=100)}
+
+    # GridSearch
+    grid_search_gau = GridSearchCV(
+        estimator=gau,
+        param_grid=parameters_gau,
+        cv=10,  # use any cross validation technique
+        verbose=1,
+        scoring='accuracy'
+    )
+
+    gau_1 = grid_search_gau.fit(x_train, y_train)
+    y_pred_gau1 = gau_1.predict(x_test)
+    print("Best params", grid_search_gau.best_params_)
+    '''
 
     gau = GaussianNB(var_smoothing=0.4329)
 
@@ -39,6 +76,23 @@ def gaussian_nb_classification(training, target):
 def random_forest_classification(training, target):
 
     x_train, x_test, y_train, y_test = train_test_split(training, target, test_size=0.3, random_state=0)
+    '''
+    rf = RandomForestClassifier()
+
+    parameters_rf = {
+        'n_estimators': (100, 500, 1300),
+        'max_depth': (15, 25),
+        'min_samples_split': (3, 15),
+        'min_samples_leaf': (1, 3)
+
+    }
+
+    grid_rf = GridSearchCV(rf, parameters_rf, cv=10, verbose=1,
+                         scoring='accuracy')
+    rf1 = grid_rf.fit(x_train, y_train)
+    y_pred_rf1 = rf1.predict(x_test)
+    print("Best params", grid_rf.best_params_)
+    '''
 
     rf = RandomForestClassifier(max_depth=15, max_features=3, min_samples_leaf=3, min_samples_split=3,
                                 n_estimators=1300, criterion='entropy')
